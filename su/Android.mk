81f690e1440eb572755c8390e110eb277eb415c3
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-02-08 16:05:59 +0800

    su: import CyanogenMod's version for Superuser
    From git://github.com/CyanogenMod/android_system_su

--- su/Android.mk
+++ su/Android.mk
@@ -1,15 +1,23 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= su.c
+LOCAL_SRC_FILES := \
+    activity.cpp \
+    db.c \
+    su.c\
 
-LOCAL_MODULE:= su
+LOCAL_MODULE := su
 
-LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_C_INCLUDES += external/sqlite/dist
 
-LOCAL_STATIC_LIBRARIES := libc
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libsqlite \
+    libcutils \
+    libbinder \
+    libutils \
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
