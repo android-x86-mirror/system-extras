9485e34c47b651aee323303928d7e445d13e4bc0
Chih-Wei Huang <cwhuang@linux.org.tw>
2019-02-26 21:59:39 +0800

    mkuserimg_mke2fs.sh: calculate size automatically
    Add extra size by the $SRC_DIR size plus 10%.

--- ext4_utils/mkuserimg_mke2fs.sh
+++ ext4_utils/mkuserimg_mke2fs.sh
@@ -160,6 +160,9 @@ fi
 if [ -z $SIZE ]; then
   echo "Need size of filesystem"
   exit 2
+elif [ 0 -eq 0$SIZE ]; then
+  s=$(du -sm $SRC_DIR | cut -f1)
+  SIZE=$(($s / 10 + $s))M
 fi
 
 # Round down the filesystem length to be a multiple of the block size
